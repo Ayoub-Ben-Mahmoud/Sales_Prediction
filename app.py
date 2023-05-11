13cbd0d0b4853fa98d28bbd2c2673645e2e0ca89
Ayoub Ben Mahmoud <ayoubb917@gmail.com>
2023-05-11 15:56:48 +0100

    Removed .gitignore file

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS,cross_origin
+import pickle
+import numpy as numpy
+
+
+app=Flask(__name__)
+model = pickle.load(open('xgbmodel.pkl','rb'))
+scaler = pickle.load(open('scaler.pickle','rb'))
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
+@app.errorhandler(404)
+
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''For rendering results on HTML GUI'''
+    int_features = [int(x) for x in request.form.values()]
+    pre_final_features = [np.array(int_features)]
+    final_features = scaler.transform(pre_final_features)
+    prediction = model.predict(final_features)
+    print('Prediction Value is',prediction[0])
+    output=prediction[0]
+
+    return render_template('index.html',prediction_text='This user will buy from social network ad {}'.format(output))
+
+    if __name__ == "__main__":
+        app.run(debug=True)
